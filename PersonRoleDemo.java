b2b2914dffd6304565927dbec87e61c265ed7798
vaishnavidahale-cyber <vaishnavidahale29@gmail.com>
2026-03-16 23:41:03 +0530

    PersonRoleDemo code

---
+++ PersonRoleDemo.java
@@ -0,0 +1,33 @@
+class Person {
+    void role() {
+        System.out.println("I am a person");
+    }
+}
+
+class Employee extends Person {
+    void role() {
+        System.out.println("I am an employee");
+    }
+}
+
+class Manager extends Employee {
+    void role() {
+        System.out.println("I am a manager");
+    }
+}
+
+public class PersonRoleDemo {
+    public static void main(String[] args) {
+
+        Person p;
+
+        p = new Person();
+        p.role();
+
+        p = new Employee();
+        p.role();
+
+        p = new Manager();
+        p.role();
+    }
+}
