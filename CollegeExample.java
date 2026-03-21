d8f83d446f11aabd7084547e8afddb1a63961599
vaishnavidahale-cyber <vaishnavidahale29@gmail.com>
2026-03-21 23:22:10 +0530

    Added program to check if string contains 'College' using contains() method

---
+++ CollegeExample.java
@@ -0,0 +1,10 @@
+public class CollegeExample {
+    public static void main(String[] args) {
+        String str = "Welcome to College";
+
+        if(str.contains("College"))
+            System.out.println("Contains 'College'");
+        else
+            System.out.println("Not found");
+    }
+}
