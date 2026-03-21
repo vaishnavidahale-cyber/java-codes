0f127dda8ddc6c8cb880b757c617182ec8bcd83b
vaishnavidahale-cyber <vaishnavidahale29@gmail.com>
2026-03-21 23:34:34 +0530

    Added program to find index of character 'e' using indexOf() method

---
+++ IndexExample.java
@@ -0,0 +1,9 @@
+public class IndexExample {
+    public static void main(String[] args) {
+        String str = "Welcome";
+
+        int index = str.indexOf('e');
+
+        System.out.println("Index of 'e': " + index);
+    }
+}
