374d6be621bdd65d9dd3244ced64c98d21e8a6b5
vaishnavidahale-cyber <vaishnavidahale29@gmail.com>
2026-03-21 23:26:36 +0530

    Added program to check if string contains 'College' using contains() method

---
+++ ContainsExample2.java
@@ -0,0 +1,10 @@
+public class ContainsExample2{
+    public static void main(String[] args) {
+        String str = "Online Learning Platform";
+
+        if(str.contains("Offline"))
+            System.out.println("Contains 'Offline'");
+        else
+            System.out.println("Does not contain 'Offline'");
+    }
+}
