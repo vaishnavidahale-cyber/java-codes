09d4b7146fc6fce7857d8e73b046fc1a10114cdc
vaishnavidahale-cyber <vaishnavidahale29@gmail.com>
2026-03-30 22:54:09 +0530

    Added StudentInputSystem with multiple catch blocks

---
+++ StudentInputSystem.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class StudentInputSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            int age = sc.nextInt();
+            if(age <= 0)
+                throw new ArithmeticException();
+
+            System.out.println("Student age is: " + age);
+        } catch(InputMismatchException e) {
+            System.out.println("Invalid input");
+        } catch(ArithmeticException e) {
+            System.out.println("Invalid input");
+        }
+    }
+}
