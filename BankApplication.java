811392db3be87e6644280da02cd1e5b41bcda388
vaishnavidahale-cyber <vaishnavidahale29@gmail.com>
2026-03-31 21:02:18 +0530

    Added BankApplication using throws keyword

---
+++ BankApplication.java
@@ -0,0 +1,15 @@
+class BankApplication {
+
+    static void processTransaction() throws Exception {
+        throw new Exception("Transaction failed");
+    }
+
+    public static void main(String[] args) {
+        try {
+            processTransaction();
+            System.out.println("Transaction processed");
+        } catch(Exception e) {
+            System.out.println("Exception handled in main");
+        }
+    }
+}
