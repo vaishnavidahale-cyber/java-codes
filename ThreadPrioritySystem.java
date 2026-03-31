b3593daa9478d7ce2085d13b71aa4ea8a8c2df5b
vaishnavidahale-cyber <vaishnavidahale29@gmail.com>
2026-03-31 21:04:55 +0530

    Added ThreadPrioritySystem demonstrating priorities

---
+++ ThreadPrioritySystem.java
@@ -0,0 +1,18 @@
+class ThreadPrioritySystem extends Thread {
+
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " running");
+    }
+
+    public static void main(String[] args) {
+
+        ThreadPrioritySystem t1 = new ThreadPrioritySystem();
+        ThreadPrioritySystem t2 = new ThreadPrioritySystem();
+
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.MAX_PRIORITY);
+
+        t1.start();
+        t2.start();
+    }
+}
