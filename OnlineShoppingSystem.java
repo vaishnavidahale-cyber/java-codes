c337f85ad23ba4ad2ffc438c56df40721a885aa9
vaishnavidahale-cyber <vaishnavidahale29@gmail.com>
2026-03-31 21:03:24 +0530

    Added OnlineShoppingSystem with multiple threads

---
+++ OnlineShoppingSystem.java
@@ -0,0 +1,16 @@
+class OnlineShoppingSystem {
+
+    public static void main(String[] args) {
+
+        Thread payment = new Thread(() -> {
+            System.out.println("Processing payment...");
+        });
+
+        Thread order = new Thread(() -> {
+            System.out.println("Order confirmed...");
+        });
+
+        payment.start();
+        order.start();
+    }
+}
