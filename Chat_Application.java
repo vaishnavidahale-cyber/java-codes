824c4d0a22430220e0b1fc5103590db315d73c46
vaishnavidahale-cyber <vaishnavidahale29@gmail.com>
2026-04-01 22:13:04 +0530

    Added ChatApplication demonstrating thread lifecycle

---
+++ Chat_Application.java
@@ -0,0 +1,22 @@
+class Chat_Application {
+
+    static class Sender extends Thread {
+        public void run() {
+            System.out.println("Sending message...");
+        }
+    }
+
+    static class Receiver extends Thread {
+        public void run() {
+            System.out.println("Receiving message...");
+        }
+    }
+
+    public static void main(String[] args) {
+        Sender s = new Sender();
+        Receiver r = new Receiver();
+
+        s.start();
+        r.start();
+    }
+}
