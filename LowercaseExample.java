d264e77c98fc0fda3b76660caf7fead84f35657f
vaishnavidahale-cyber <vaishnavidahale29@gmail.com>
2026-04-02 23:41:53 +0530

    Added program to convert 'COMPUTER SCIENCE DEPARTMENT' into lowercase using toLowerCase() method

---
+++ LowercaseExample.java
@@ -0,0 +1,8 @@
+public class LowercaseExample {
+    public static void main(String[] args) {
+        String text = "COMPUTER SCIENCE DEPARTMENT";
+        String result = text.toLowerCase();
+
+        System.out.println("Lowercase: " + result);
+    }
+}
