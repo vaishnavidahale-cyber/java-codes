99d9e37b9292cb469778b7f23c5d7d10b56feae8
vaishnavidahale-cyber <vaishnavidahale29@gmail.com>
2026-04-02 23:36:51 +0530

    Added program to convert 'welcome to java' into uppercase using toUpperCase() method

---
+++ UppercaseExample.java
@@ -0,0 +1,8 @@
+public class UppercaseExample {
+    public static void main(String[] args) {
+        String text = "welcome to java";
+        String result = text.toUpperCase();
+
+        System.out.println("Uppercase: " + result);
+    }
+}
